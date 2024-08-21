f2f910fa457b0f71b83ff35912fa9bf6d9f1ff34
gil12av <117095291+gil12av@users.noreply.github.com>
2024-08-21 11:22:57 +0300

    Update symbol_table.c

--- symbol_table.c
+++ symbol_table.c
@@ -16,17 +16,11 @@ SymbolTable* create_symbol_table() {
     
 }
 
-char *handler_symbolName(char* name) {
-  return name;
-}
-char *handler_symbolType(char* type) {
- return type;
-}
-
 void handler(char *name2, char *type2) {
-printf("\n FROM HANDLER ------- > name: %s, type = %s\n", name2, type2);
+printf("\n Function from handler ------- > name: %s, type = %s\n", name2, type2);
   if ( name2 == NULL)
-    name2 = strdup("fuckoff");
+    name2 = strdup(" ");
+    //type2 = strdup(" ");
   if ( type2 == NULL )
     add_symbol(table, strdup(name2), strdup(type_c) );
   else
@@ -36,20 +30,17 @@ printf("\n FROM HANDLER ------- > name: %s, type = %s\n", name2, type2);
     type_c = strdup(type2);
 }
 // הכנסה של משתנה / פונקציה לטבלת הסמלים
-void add_symbol(SymbolTable* table, char* name, const char* type) {
+void add_symbol(SymbolTable* table, char* name, char* type) {
     Symbol* new_symbol = (Symbol*)malloc(sizeof(Symbol));
     new_symbol->name = strdup(name);
     new_symbol->type = strdup(type);
     new_symbol->next = table->currentScope->symbols;
     table->currentScope->symbols = new_symbol;
 
-    // הדפסה לאחר הוספת הסמל, כאשר name ו-type כבר מוגדרים
-    printf("Added symbol - Name: %s, Type: %s\n", name, type);
-     // הדפסת הסקופ הנוכחי לאחר הוספת סמל
+    printf("this is from add _symbol function --> Added symbol - Name: %s, Type: %s\n", name, type);
 }
 
-
-
+  
 // חיפוש בתוך הסקופ הפנימי ויציאה לחיפוש בסקופ מעליו - הורים. בצורה הזו נבטיח שנגיע מהחלק המקונן ביותר עד לשורש
 Symbol* find_symbol(SymbolTable* table, char* name) {
     Scope* scope = table->currentScope;
@@ -68,12 +59,14 @@ Symbol* find_symbol(SymbolTable* table, char* name) {
 
 // יצירת סקופ כשנכנסים לפונקציה או לבלוק חדש.
 void enter_scope(SymbolTable* table) {
+    static int scope_id = 1;
     Scope* new_scope = (Scope*)malloc(sizeof(Scope));
     new_scope->symbols = NULL;
     new_scope->parent = table->currentScope;
     new_scope->next = NULL;
+    new_scope->scope_id = scope_id++;
     table->currentScope = new_scope;
-    printf("Entered new scope\n");
+    printf("---- End of %d scope ---\n", new_scope->scope_id);
 }
 
 
@@ -84,13 +77,23 @@ void exit_scope(SymbolTable* table) {
 }
 
 void print_current_scope(SymbolTable* table) {
-    printf("Printing Current Scope:\n");
     Scope* scope = table->currentScope;
+    int depth = 0;
+    while (scope != NULL) {
+        depth++;
+        scope = scope->parent;
+    }
+
+    scope = table->currentScope;
+    printf("Scope Level: %d\n", depth);
+    printf("-------------\n");
     Symbol* symbol = scope->symbols;
     while (symbol != NULL) {
-        printf("  Name: %s, Type: %s\n", symbol->name, symbol->type);
+        printf("| %-15s | %-15s |\n", symbol->name, symbol->type);
         symbol = symbol->next;
     }
+    printf("-------------\n");
+    printf("\n");
 }
 
 // פונקציה לשחרור זיכרון של טבלת הסמלים
@@ -111,4 +114,25 @@ void free_symbol_table(SymbolTable* table) {
         free(temp_scope);
     }
     free(table);
+ }   
+    
+    
+void print_all_scopes(SymbolTable* table) {
+    Scope* scope = table->currentScope;
+    int scope_level = 1;
+
+    while (scope != NULL) {
+        printf("In Scope %d:\n", scope_level);
+        printf("-------------\n");
+        Symbol* symbol = scope->symbols;
+        while (symbol != NULL) {
+            printf("| %-15s | %-15s |\n", symbol->name, symbol->type);
+            symbol = symbol->next;
+        }
+        printf("-------------\n");
+        printf("\n");
+
+        scope = scope->parent;
+        scope_level++;
+    }
 }
